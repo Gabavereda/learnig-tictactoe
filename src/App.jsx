62cf1dfa968ddee71ec8ef9fb05bb221bd10173f
Glauber Vereda <gabavereda@gmail.com>
2025-02-19 10:29:41 -0300

    create repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <button className="square">X
+        </button>
+        <button className="square">X
+        </button>
+      
+      </div>
+    </>
+  )
+}
+
+export default App
