42d790a287faf0ccae1ce85a0ab05eab5977fbb1
Glauber Vereda <gabavereda@gmail.com>
2025-02-21 20:24:30 -0300

    feat:: disapear

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 //create func square
 // pass prop
-const Square = ({value}) => {
-  return (
-    <button className="square">{value}
-    </button>
-  )
-}
+const Square = () => {
+
+  const handleClick = () => {
+    console.log(`Clicked on line ${value}`)
+  };
+
+  // making disapear
+  const [visible, setVisible] = useState(true);
+  // doing x
+
+
+  const handleRightClick = (event, _value) => {
+    event.preventDefault();
+    setVisible(false);
+  };
+  return visible ? (
+    <button
+      className="square"
+      onClick={handleClick}
+      onContextMenu={handleRightClick} // Usa onContextMenu para clique direito
+
+    >   {value}
+    </button >
+  ) : null
+};
 
 const App = () => {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div >
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div >
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div >
     </>
   )
