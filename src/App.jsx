5e29b74b706e53615ec12a1f74381f29e510fa5a
Glauber Vereda <gabavereda@gmail.com>
2025-02-21 21:15:33 -0300

    making the x functional

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,48 @@ import React, { useState } from 'react';
 
 //create func square
 // pass prop
-const Square = () => {
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >   {value}
+    </button >
+  );
+}
 
-  const handleClick = () => {
+const App = ({ value }) => {
+  
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
     console.log(`Clicked on line ${value}`)
   };
 
-  // making disapear
-  const [visible, setVisible] = useState(true);
-  // doing x
-
-
-  const handleRightClick = (event, _value) => {
-    event.preventDefault();
-    setVisible(false);
-  };
-  return visible ? (
-    <button
-      className="square"
-      onClick={handleClick}
-      onContextMenu={handleRightClick} // Usa onContextMenu para clique direito
-
-    >   {value}
-    </button >
-  ) : null
-};
-
-const App = () => {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-      </div >
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-      </div >
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
-      </div >
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </>
-  )
+  );
 }
+
 
 export default App
